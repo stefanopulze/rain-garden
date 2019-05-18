59fece28615e387d2aeb5653c93d201d617a515a
Stefano <stefano.pulze87@gmail.com>
2019-05-18 19:39:45 +0200

    Code running

--- main-node/main-node.ino
+++ main-node/main-node.ino
@@ -3,33 +3,59 @@
 #include <RF24.h>
 #include <ArduinoJson.h>
 
-#define CE_PIN 9
-#define CSN_PIN 10
-#define DEBUG_SERIAL true
+// Wemos D1
+// #define CE_PIN D2
+// #define CSN_PIN D8
 
-const uint64_t pipe = 0xE8E8F0F0E1LL;
+#define CE_PIN 4
+#define CSN_PIN 3
+#define DEBUG_SERIAL true
+#define PAYLOAD_SIZE 128
+
+struct SensorData
+{
+    float temperature;
+    float humidity;
+    float pressure;
+};
+
+const byte pipe[6] = "00001";
 
 RF24 radio(CE_PIN, CSN_PIN);
+SensorData sensorData;
 
 void setup()
 {
     Serial.begin(115200);
+    Serial.println("Avvio nodo master");
 
     radio.begin();
-    radio.openReadingPipe(0, pipe);
+    radio.setDataRate(RF24_250KBPS);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.openReadingPipe(1, pipe);
+    radio.setPayloadSize(PAYLOAD_SIZE);
     radio.startListening();
+
+    if (!radio.isChipConnected())
+    {
+        // TODO fix with led blink
+        Serial.println("Radio NOT CONNECTED");
+    }
+
+    Serial.println("Avviato");
 }
 
 void loop()
 {
     if (radio.available())
     {
-        unsigned char buffer[1024];
-        radio.read(buffer, sizeof(buffer));
+        radio.read(&sensorData, sizeof(sensorData));
 
-        DynamicJsonDocument json(1024);
-        deserializeJson(json, buffer);
-
-        //serializeJson(json, &Serial);
+        Serial.print("Temperature: ");
+        Serial.print(sensorData.temperature);
+        Serial.print(" - Humidity: ");
+        Serial.print(sensorData.humidity);
+        Serial.print(" - Pressure: ");
+        Serial.println(sensorData.pressure);
     }
 }
--- sensor-node/sensor-node.ino
+++ sensor-node/sensor-node.ino
@@ -3,13 +3,22 @@
 #include <RF24.h>
 #include <BME280I2C.h>
 #include <Wire.h>
-#include <ArduinoJson.h>
+// #include <ArduinoJson.h>
+// #include "printf.h"
 
-#define CE_PIN 9
-#define CSN_PIN 10
-#define DEBUG_SERIAL true
+#define CE_PIN 7
+#define CSN_PIN 8
+// #define DEBUG_SERIAL false
+#define PAYLOAD_SIZE 128
 
-const uint64_t pipe = 0xE8E8F0F0E1LL;
+const byte pipe[6] = "00001";
+
+struct SensorData
+{
+    float temperature;
+    float humidity;
+    float pressure;
+};
 
 RF24 radio(CE_PIN, CSN_PIN);
 BME280I2C bme;
@@ -17,9 +26,19 @@ BME280I2C bme;
 void setup()
 {
     Serial.begin(115200);
+    Serial.println("Avvio sensor node");
 
     radio.begin();
+    radio.setDataRate(RF24_250KBPS);
+    radio.setPALevel(RF24_PA_MIN);
     radio.openWritingPipe(pipe);
+    radio.setPayloadSize(PAYLOAD_SIZE);
+    radio.stopListening();
+
+    if (!radio.isChipConnected())
+    {
+        Serial.println("Radio NOT CONNECTED");
+    }
 
     Wire.begin();
     bme.begin();
@@ -29,20 +48,19 @@ void loop()
 {
     float temp(NAN), hum(NAN), pres(NAN);
     bme.read(pres, temp, hum);
+    delay(200);
 
-    // json
-    DynamicJsonDocument json(1024);
-    json["temperature"] = temp;
-    json["humidity"] = hum;
-    json["pressure"] = pres;
+    SensorData data;
+    data.pressure = pres;
+    data.humidity = hum;
+    data.temperature = temp;
+
+    radio.write(&data, sizeof(data));
 
 #ifdef DEBUG_SERIAL
-    serializeJson(json, Serial);
-    Serial.println();
+    Serial.print("Struct data size: ");
+    Serial.println(sizeof(data));
 #endif
 
-    size_t jsonLength = measureJson(json);
-    char buffer[256];
-    serializeJson(json, buffer);
-    // radio.write(buffer, jsonLength);
+    delay(3000);
 }
