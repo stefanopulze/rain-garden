67f4b5696129abfda026b08c8daa7a8f964bdf2c
Stefano <stefano.pulze87@gmail.com>
2019-05-19 12:16:23 +0200

    Add oled

--- main-node/main-node.ino
+++ main-node/main-node.ino
@@ -2,15 +2,17 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <ArduinoJson.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "printf.h"
 
-// Wemos D1
-// #define CE_PIN D2
-// #define CSN_PIN D8
+#define SSD1306_128_32
+// #define SSD1306_128_64
+#define OLED_RESET A2
 
 #define CE_PIN 4
 #define CSN_PIN 3
 #define DEBUG_SERIAL true
-#define PAYLOAD_SIZE 128
 
 struct SensorData
 {
@@ -19,22 +21,36 @@ struct SensorData
     float pressure;
 };
 
-const byte pipe[6] = "00001";
+const byte pipe[5] = {'0', '0', '0', '0', '1'};
 
 RF24 radio(CE_PIN, CSN_PIN);
+Adafruit_SSD1306 display(OLED_RESET);
 SensorData sensorData;
 
 void setup()
 {
     Serial.begin(115200);
+    printf_begin();
     Serial.println("Avvio nodo master");
 
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    // // non va a capo ma esce dallo schermo se riga troppo lunga
+    display.setTextWrap(false);
+    // // 0 luminosità massima, 1 leggermente inferiore
+    display.dim(1);
+    // svuotaDisplay();
+
+    disegnaSchermataIniziale();
+
     radio.begin();
+    radio.enableAckPayload(); // Allow optional ack payloads
     radio.setDataRate(RF24_250KBPS);
-    radio.setPALevel(RF24_PA_MIN);
+    radio.setPALevel(RF24_PA_HIGH);
     radio.openReadingPipe(1, pipe);
-    radio.setPayloadSize(PAYLOAD_SIZE);
     radio.startListening();
+     radio.writeAckPayload(1, 1, 1);
+
+    radio.printDetails();
 
     if (!radio.isChipConnected())
     {
@@ -42,14 +58,19 @@ void setup()
         Serial.println("Radio NOT CONNECTED");
     }
 
-    Serial.println("Avviato");
+    disegnaTesto("Setup", "Avviato", "...");
 }
 
 void loop()
 {
     if (radio.available())
     {
+        byte ackByte;
+
         radio.read(&sensorData, sizeof(sensorData));
+        radio.writeAckPayload(1, 0x02, 1);
+
+        disegnaSensorData(sensorData);
 
         Serial.print("Temperature: ");
         Serial.print(sensorData.temperature);
@@ -58,4 +79,76 @@ void loop()
         Serial.print(" - Pressure: ");
         Serial.println(sensorData.pressure);
     }
+}
+
+void svuotaDisplay()
+{
+    display.clearDisplay();
+    display.display();
+}
+
+void disegnaSensorData(SensorData data)
+{
+    display.clearDisplay();
+    display.display();
+
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(10, 5);
+    display.print("Temp:");
+    display.println(data.temperature);
+
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(10, 15);
+    display.print("Humi:");
+    display.println(data.humidity);
+
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(10, 25);
+    display.print("Press:");
+    display.println(data.pressure);
+
+    display.display();
+}
+
+void disegnaSchermataIniziale()
+{
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(10, 5);
+    display.println("ENERGY");
+
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(10, 15);
+    display.println("MONITOR");
+
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(10, 45);
+    display.println("V 0.1.6 - 2018");
+    display.display();
+}
+
+void disegnaTesto(String line1, String line2, String line3)
+{
+    display.clearDisplay();
+
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(10, 5);
+    display.println(line1);
+
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(10, 15);
+    display.println(line2);
+
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(10, 45);
+    display.println(line3);
+    display.display();
 }
--- sensor-node/sensor-node.ino
+++ sensor-node/sensor-node.ino
@@ -3,21 +3,22 @@
 #include <RF24.h>
 #include <BME280I2C.h>
 #include <Wire.h>
+#include "printf.h"
 // #include <ArduinoJson.h>
 // #include "printf.h"
 
 #define CE_PIN 7
 #define CSN_PIN 8
-// #define DEBUG_SERIAL false
-#define PAYLOAD_SIZE 128
+#define DEBUG_SERIAL false
+#define STATUS_PIN 43
 
-const byte pipe[6] = "00001";
+const byte pipe[5] = {'0', '0', '0', '0', '1'};
 
 struct SensorData
 {
-    float temperature;
-    float humidity;
-    float pressure;
+  float temperature;
+  float humidity;
+  float pressure;
 };
 
 RF24 radio(CE_PIN, CSN_PIN);
@@ -25,42 +26,74 @@ BME280I2C bme;
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("Avvio sensor node");
+  Serial.begin(115200);
+  printf_begin();
+  Serial.println("Avvio sensor node");
+  pinMode(STATUS_PIN, OUTPUT);
+  digitalWrite(STATUS_PIN, LOW);
 
-    radio.begin();
-    radio.setDataRate(RF24_250KBPS);
-    radio.setPALevel(RF24_PA_MIN);
-    radio.openWritingPipe(pipe);
-    radio.setPayloadSize(PAYLOAD_SIZE);
-    radio.stopListening();
+  radio.begin();
+  radio.setAutoAck(true);   // Ensure autoACK is enabled
+  radio.enableAckPayload(); // Allow optional ack payloads
+  radio.setRetries(3, 5);   // delay, count
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.openWritingPipe(pipe);
+  radio.stopListening();
 
-    if (!radio.isChipConnected())
-    {
-        Serial.println("Radio NOT CONNECTED");
-    }
+  radio.printDetails();
 
-    Wire.begin();
-    bme.begin();
+  if (!radio.isChipConnected())
+  {
+    Serial.println("Radio NOT CONNECTED");
+    digitalWrite(STATUS_PIN, HIGH);
+  }
+
+  // radio.printDetails();
+
+  Wire.begin();
+  bme.begin();
 }
 
 void loop()
 {
-    float temp(NAN), hum(NAN), pres(NAN);
-    bme.read(pres, temp, hum);
-    delay(200);
+  float temp(NAN), hum(NAN), pres(NAN);
+  bme.read(pres, temp, hum);
+  delay(200);
 
-    SensorData data;
-    data.pressure = pres;
-    data.humidity = hum;
-    data.temperature = temp;
+  SensorData data;
+  data.pressure = pres;
+  data.humidity = hum;
+  data.temperature = temp;
+  byte ackByte;
 
-    radio.write(&data, sizeof(data));
+  bool rslt;
+  rslt = radio.write(&data, sizeof(data));
+  if (rslt)
+  {
+    if (radio.isAckPayloadAvailable())
+    {
+      radio.read(&ackByte, 1);
+      Serial.print("Acknowledge received: ");
+      Serial.println(ackByte);
+    }
+    else
+    {
+      Serial.println("Acknowledge but no data ");
+    }
+
+    digitalWrite(STATUS_PIN, LOW);
+  }
+  else
+  {
+    Serial.println("Tx failed");
+    digitalWrite(STATUS_PIN, HIGH);
+  }
 
 #ifdef DEBUG_SERIAL
-    Serial.print("Struct data size: ");
-    Serial.println(sizeof(data));
+  Serial.print("Struct data size: ");
+  Serial.println(sizeof(data));
 #endif
 
-    delay(3000);
+  delay(3000);
 }
